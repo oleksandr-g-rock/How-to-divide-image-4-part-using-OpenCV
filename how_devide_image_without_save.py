f64436798e2a81f07cf0d73121d997ae6c5efb54
Alex G <32989220+oleksandr-g-rock@users.noreply.github.com>
2020-12-12 23:08:02 -0600

    Update how_devide_image_without_save.py

--- how_devide_image_without_save.py
+++ how_devide_image_without_save.py
@@ -29,13 +29,13 @@ l2 = img[:, :width_cutoff]
 l1 = img[:, width_cutoff:]
 # finish vertical devide image
 #rotate image to 90 COUNTERCLOCKWISE
-l1 = cv2.rotate(l1, cv2.ROTATE_90_COUNTERCLOCKWISE)
-#save
-cv2.imwrite("part_left_1.jpg", l1)
-#rotate image to 90 COUNTERCLOCKWISE
 l2 = cv2.rotate(l2, cv2.ROTATE_90_COUNTERCLOCKWISE)
 #save
 cv2.imwrite("part_left_2.jpg", l2)
+#rotate image to 90 COUNTERCLOCKWISE
+l1 = cv2.rotate(l1, cv2.ROTATE_90_COUNTERCLOCKWISE)
+#save
+cv2.imwrite("part_left_1.jpg", l1)
 
 ##########################################
 # At first Horizontal devide right1 image#
@@ -47,15 +47,15 @@ height = img.shape[0]
 width = img.shape[1]
 # Cut the image in half
 width_cutoff = width // 2
-r2 = img[:, :width_cutoff]
-r1 = img[:, width_cutoff:]
+r4 = img[:, :width_cutoff]
+r3 = img[:, width_cutoff:]
 # finish vertical devide image
 #rotate image to 90 COUNTERCLOCKWISE
-r1 = cv2.rotate(r1, cv2.ROTATE_90_COUNTERCLOCKWISE)
+r4 = cv2.rotate(r4, cv2.ROTATE_90_COUNTERCLOCKWISE)
 #save
-cv2.imwrite("part_right_3.jpg", r1)
+cv2.imwrite("part_right_4.jpg", r4)
 #rotate image to 90 COUNTERCLOCKWISE
-r2 = cv2.rotate(r2, cv2.ROTATE_90_COUNTERCLOCKWISE)
+r3 = cv2.rotate(r3, cv2.ROTATE_90_COUNTERCLOCKWISE)
 #save
-cv2.imwrite("part_right_4.jpg", r2)
+cv2.imwrite("part_right_3.jpg", r3)
 
