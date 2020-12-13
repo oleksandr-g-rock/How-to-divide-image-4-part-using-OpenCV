0bc35e06a194e1fbbccd56ce6c9f7ac538997a3c
Alex G <32989220+oleksandr-g-rock@users.noreply.github.com>
2020-12-12 22:23:08 -0600

    Update how_devide_image_without_save.py

--- how_devide_image_without_save.py
+++ how_devide_image_without_save.py
@@ -1,46 +1,61 @@
-#Set vertical devide
 import cv2   
-# Read the image
+
+# load image
 img = cv2.imread('image_test.jpg')
-print(img.shape)
+
+##########################################
+# At first vertical devide image         #
+##########################################
+# start vertical devide image
 height = img.shape[0]
 width = img.shape[1]
 # Cut the image in half
 width_cutoff = width // 2
 left1 = img[:, :width_cutoff]
 right1 = img[:, width_cutoff:]
+# finish vertical devide image
 
-#############################################
+##########################################
+# At first Horizontal devide left1 image #
+##########################################
+#rotate image LEFT1 to 90 CLOCKWISE
 img = cv2.rotate(left1, cv2.ROTATE_90_CLOCKWISE)
-####
-print(img.shape)
+# start vertical devide image
 height = img.shape[0]
 width = img.shape[1]
 # Cut the image in half
 width_cutoff = width // 2
 l1 = img[:, :width_cutoff]
 l2 = img[:, width_cutoff:]
-#now image vertical
+# finish vertical devide image
+#rotate image to 90 COUNTERCLOCKWISE
 l1 = cv2.rotate(l1, cv2.ROTATE_90_COUNTERCLOCKWISE)
+#save
 cv2.imwrite("one_horisont_1.jpg", l1)
-#
+#rotate image to 90 COUNTERCLOCKWISE
 l2 = cv2.rotate(l2, cv2.ROTATE_90_COUNTERCLOCKWISE)
+#save
 cv2.imwrite("one_horisont_2.jpg", l2)
 
-
-#############################################
+##########################################
+# At first Horizontal devide right1 image#
+##########################################
+#rotate image RIGHT1 to 90 CLOCKWISE
 img = cv2.rotate(right1, cv2.ROTATE_90_CLOCKWISE)
-####
-print(img.shape)
+# start vertical devide image
 height = img.shape[0]
 width = img.shape[1]
 # Cut the image in half
 width_cutoff = width // 2
 r1 = img[:, :width_cutoff]
 r2 = img[:, width_cutoff:]
-#now image vertical
+# finish vertical devide image
+#rotate image to 90 COUNTERCLOCKWISE
 r1 = cv2.rotate(r1, cv2.ROTATE_90_COUNTERCLOCKWISE)
+#save
 cv2.imwrite("second_vhorisont_1.jpg", r1)
-#
+#rotate image to 90 COUNTERCLOCKWISE
 r2 = cv2.rotate(r2, cv2.ROTATE_90_COUNTERCLOCKWISE)
+#save
 cv2.imwrite("second_horisont_2.jpg", r2)
+
